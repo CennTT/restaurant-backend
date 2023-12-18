6125c4a258bbc069815767ee4011df3d39e99902
Vincent Waiman <vincentwn2@gmail.com>
2023-12-18 23:21:57 +0700

    update sql

--- restaurant.sql
+++ restaurant.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Dec 18, 2023 at 06:11 AM
--- Server version: 10.4.28-MariaDB
--- PHP Version: 8.1.17
+-- Host: 127.0.0.1:3306
+-- Generation Time: Dec 18, 2023 at 01:18 PM
+-- Server version: 5.7.36
+-- PHP Version: 7.4.26
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -27,9 +27,11 @@ SET time_zone = "+00:00";
 -- Table structure for table `alembic_version`
 --
 
-CREATE TABLE `alembic_version` (
-  `version_num` varchar(32) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+DROP TABLE IF EXISTS `alembic_version`;
+CREATE TABLE IF NOT EXISTS `alembic_version` (
+  `version_num` varchar(32) NOT NULL,
+  PRIMARY KEY (`version_num`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `alembic_version`
@@ -44,56 +46,36 @@ INSERT INTO `alembic_version` (`version_num`) VALUES
 -- Table structure for table `food_and_beverage`
 --
 
-CREATE TABLE `food_and_beverage` (
-  `id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `food_and_beverage`;
+CREATE TABLE IF NOT EXISTS `food_and_beverage` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(100) DEFAULT NULL,
   `type` varchar(20) DEFAULT NULL,
   `price` float DEFAULT NULL,
-  `path` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `path` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=44 DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `food_and_beverage`
 --
 
 INSERT INTO `food_and_beverage` (`id`, `name`, `type`, `price`, `path`) VALUES
-(1, 'Pizza', 'food', 70000, NULL),
-(2, 'Burger', 'food', NULL, NULL),
-(3, 'Salad', 'food', NULL, NULL),
-(4, 'Pasta', 'food', NULL, NULL),
-(5, 'Sandwich', 'food', NULL, NULL),
-(6, 'Steak', 'food', NULL, NULL),
-(7, 'Sushi', 'food', NULL, NULL),
-(8, 'Tacos', 'food', NULL, NULL),
-(9, 'Fried Chicken', 'food', NULL, NULL),
-(10, 'Ramen', 'food', NULL, NULL),
-(11, 'Lasagna', 'food', NULL, NULL),
-(12, 'Curry', 'food', NULL, NULL),
-(13, 'Burrito', 'food', NULL, NULL),
-(14, 'Grilled Cheese', 'food', NULL, NULL),
-(15, 'Pad Thai', 'food', NULL, NULL),
-(16, 'Coffee', 'beverage', NULL, NULL),
-(17, 'Tea', 'beverage', NULL, NULL),
-(18, 'Soda', 'beverage', NULL, NULL),
-(19, 'Juice', 'beverage', NULL, NULL),
-(20, 'Smoothie', 'beverage', NULL, NULL),
-(21, 'Beer', 'beverage', NULL, NULL),
-(22, 'Wine', 'beverage', NULL, NULL),
-(23, 'Cocktail', 'beverage', NULL, NULL),
-(24, 'Lemonade', 'beverage', NULL, NULL),
-(25, 'Milkshake', 'beverage', NULL, NULL),
-(26, 'Iced Tea', 'beverage', NULL, NULL),
-(27, 'Hot Chocolate', 'beverage', NULL, NULL),
-(28, 'Limeade', 'beverage', NULL, NULL),
-(29, 'Iced Coffee', 'beverage', NULL, NULL),
-(30, 'Margarita', 'beverage', NULL, NULL),
-(34, 'Babi Guling', 'food', NULL, NULL),
-(35, 'Ayam Betutu', 'food', 15000, NULL),
-(38, 'Ikan Nyatnyat', 'food', NULL, NULL),
-(39, 'Plecing Kangkung', 'food', 5000, NULL),
-(40, 'Ayam Geprek', 'food', NULL, NULL),
-(42, 'Sate Babi', 'food', 15000, NULL),
-(43, 'Nasi Kuning', 'food', 5000, NULL);
+(1, 'Pizza', 'food', 70000, 'foods/pizza.png'),
+(2, 'Burger', 'food', 36000, 'foods/burger.png'),
+(3, 'Salad', 'food', 25000, 'foods/salad.png'),
+(4, 'Pasta', 'food', 35000, 'foods/pasta.png'),
+(6, 'Steak', 'food', 45000, 'foods/steak.png'),
+(7, 'Sushi', 'food', 30000, 'foods/sushi.png'),
+(8, 'Tacos', 'food', 20000, 'foods/tacos.png'),
+(10, 'Ramen', 'food', 30000, 'foods/ramen.png'),
+(16, 'Coffee', 'beverage', 10000, 'beverages/coffee.png'),
+(17, 'Tea', 'beverage', 10000, 'beverages/tea.png'),
+(18, 'Soda', 'beverage', 7000, 'beverages/soda.png'),
+(21, 'Beer', 'beverage', 12000, 'beverages/beer.png'),
+(24, 'Lemonade', 'beverage', 12000, 'beverages/lemonade.png'),
+(25, 'Milkshake', 'beverage', 15000, 'beverages/milkshake.png'),
+(27, 'Hot Chocolate', 'beverage', 13000, 'beverages/hot_chocolate.png');
 
 -- --------------------------------------------------------
 
@@ -101,12 +83,14 @@ INSERT INTO `food_and_beverage` (`id`, `name`, `type`, `price`, `path`) VALUES
 -- Table structure for table `order`
 --
 
-CREATE TABLE `order` (
-  `id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `order`;
+CREATE TABLE IF NOT EXISTS `order` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `table_number` int(11) DEFAULT NULL,
   `status` varchar(20) DEFAULT NULL,
-  `total_price` float DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `total_price` float DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `order`
@@ -122,12 +106,16 @@ INSERT INTO `order` (`id`, `table_number`, `status`, `total_price`) VALUES
 -- Table structure for table `order_item`
 --
 
-CREATE TABLE `order_item` (
-  `id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `order_item`;
+CREATE TABLE IF NOT EXISTS `order_item` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `order_id` int(11) DEFAULT NULL,
   `item_id` int(11) DEFAULT NULL,
-  `quantity` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `quantity` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `item_id` (`item_id`),
+  KEY `order_id` (`order_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `order_item`
@@ -139,58 +127,6 @@ INSERT INTO `order_item` (`id`, `order_id`, `item_id`, `quantity`) VALUES
 (3, 1, 4, 1),
 (4, 2, 1, 3),
 (5, 2, 3, 3);
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `alembic_version`
---
-ALTER TABLE `alembic_version`
-  ADD PRIMARY KEY (`version_num`);
-
---
--- Indexes for table `food_and_beverage`
---
-ALTER TABLE `food_and_beverage`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `order`
---
-ALTER TABLE `order`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `order_item`
---
-ALTER TABLE `order_item`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `item_id` (`item_id`),
-  ADD KEY `order_id` (`order_id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `food_and_beverage`
---
-ALTER TABLE `food_and_beverage`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=44;
-
---
--- AUTO_INCREMENT for table `order`
---
-ALTER TABLE `order`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- AUTO_INCREMENT for table `order_item`
---
-ALTER TABLE `order_item`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- Constraints for dumped tables
